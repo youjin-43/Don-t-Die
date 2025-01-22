4cbe0361904d4720a2be5910bd84b9e26791097b
youjin43 <wlwlfdl3324@naver.com>
2025-01-22 14:31:43 +0900

    몬스터 스폰되고 랜덤으로 움직이는거 구현중에 충돌난거 해결

--- Assets/02_Script/Monster/MonsterBase.cs
+++ Assets/02_Script/Monster/MonsterBase.cs
@@ -5,33 +5,13 @@ public abstract class MonsterBase : MonoBehaviour
 {
     
     public MonsterData monsterData;
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
     [Header("Monster Base Attributes")]
->>>>>>> Stashed changes
-=======
-    [Header("Monster Base Attributes")]
->>>>>>> Stashed changes
     public string MonsterName;
     public float CurrentHp;
     public float MoveSpeed;
     public float AttackDamage;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    //TODO : dropItems 추가 
-
-    public abstract void Move(Vector3 targetPosition);
-
-    public abstract void Attack(GameObject target); // 공격 로직 (구체적 동작은 각 몬스터가 구현)
-=======
-=======
->>>>>>> Stashed changes
     public BiomeType MybiomeType;
-    
-
-    //TODO : dropItems 추가
+    //TODO : dropItems 추가 
 
     protected void SetData()
     {
@@ -43,10 +23,7 @@ public abstract class MonsterBase : MonoBehaviour
     }
 
     public abstract void Move();
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+    //public abstract void Attack(GameObject target); // 공격 로직 (구체적 동작은 각 몬스터가 구현)
 
     public virtual void TakeDamage(float damage)
     {
--- Assets/02_Script/Monster/MonsterSpawnManager.cs
+++ Assets/02_Script/Monster/MonsterSpawnManager.cs
@@ -42,86 +42,86 @@ public class MonsterSpawnManager : MonoBehaviour
         Init();
     }
 
-    private void Start()
-    {
-        //InitializeBiomeMonsters();
-    }
-
-
     [SerializeField] float spawnRadius = 10f;
     [SerializeField] int MinSpawnCnt = 3;
     [SerializeField] int MaxSpawnCnt = 20;
 
-    public SerializedDictionary<BiomeType, List<Vector3>> seedPoints;
+    public SerializedDictionary<BiomeType, List<Vector3>> seedPoints; // 디버그용 
     public SerializedDictionary<BiomeType, List<GameObject>> MonsterPrefab;
-    public SerializedDictionary<BiomeType, List<GameObject>> SpawnedMonster;
+    public SerializedDictionary<BiomeType, List<GameObject>> SpawnedMonster; // 디버그용 
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    
-=======
-=======
->>>>>>> Stashed changes
-    //private void OnDrawGizmos()
-    //{
-    //    foreach (var kvp in seedPoints)
-    //    {
-    //        BiomeType biome = kvp.Key;        // 현재 키 (BiomeType)
-    //        List<Vector3> points = kvp.Value; // 현재 값 (List<Vector3>)
-
-    //        foreach (var pos in points)
-    //        {
-    //            Gizmos.DrawSphere(pos, 5f);
-    //        }
-    //    }
-    //}
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
-    public void InitializeBiomeMonsters()
+    private void OnDrawGizmos()
     {
-        DebugController.Log("InitializeBiomeMonsters 실행됨");
-        seedPoints = EnvironmentManager.Instance.seedPoints;
-        DebugController.Log(seedPoints.Count.ToString());
-
-        // 각 바이옴 별로 몬스터 무리 생성
-        int tmp = 0;
         foreach (var kvp in seedPoints)
         {
             BiomeType biome = kvp.Key;        // 현재 키 (BiomeType)
             List<Vector3> points = kvp.Value; // 현재 값 (List<Vector3>)
 
+            foreach (var pos in points)
+            {
+                Gizmos.DrawSphere(pos, 5f);
+            }
+        }
+    }
+
+    public void InitializeBiomeMonsters()
+    {
+        DebugController.Log("InitializeBiomeMonsters 실행됨");
+
+        seedPoints = EnvironmentManager.Instance.seedPoints;
+
+        // 각 바이옴 별로 몬스터 무리 생성
+        foreach (var kvp in seedPoints)
+        {
+            BiomeType biome = kvp.Key;        // 현재 키 (BiomeType)
+            List<Vector3> points = kvp.Value; // 현재 값 (List<Vector3>)
             DebugController.Log($"Biome: {biome}, Seed Points Count: {points.Count}");
 
-            for(int i=0;i< points.Count; i++)
+            List<GameObject> monsters = MonsterPrefab[biome]; //선택된 바이옴에서 스폰될 수 있는 몬스터 리스트 
+
+            for (int i = 0; i < points.Count; i++)
             {
                 //현재 포인트가 현재 키 바이옴과 일치한다면(가끔 포인트가 바다 위에 있는 경우가 있음)
-                if (EnvironmentManager.Instance.biomeMap.GetTileBiome(new Vector2Int((int)points[i].x, (int)points[i].y))== biome)
+                if (EnvironmentManager.Instance.biomeMap.GetTileBiome(new Vector2Int((int)points[i].x, (int)points[i].y)) == biome)
                 {
-                    DebugController.Log($"Biome: {biome}, Seed Points: {points[i]}");
+                    DebugController.Log($"Biome: {biome}, Seed Points: {points[i]} 에 몬스터 무리 생성");
+
                     // 각 seed point를 중심으로 무작위 위치에 여러 몬스터 스폰
                     int monsterCount = Random.Range(MinSpawnCnt, MaxSpawnCnt); // 스폰할 몬스터 수 (조정 가능)
+                    int monsterPrefabIdx = 0; // 스폰 될 수 있는 몬스터 중 랜덤으로 생성
+
                     for (int m = 0; m < monsterCount; m++)
                     {
                         // 스폰 반경 내에서 랜덤 위치 계산
-                        Vector3 randomOffset = new Vector3(Random.Range(-spawnRadius, spawnRadius),Random.Range(-spawnRadius, spawnRadius),0);
+                        Vector3 randomOffset = new Vector3(Random.Range(-spawnRadius, spawnRadius), Random.Range(-spawnRadius, spawnRadius), 0);
                         Vector3 spawnPosition = points[i] + randomOffset;
 
                         // 스폰 위치가 맵 내부인지 확인 
                         Vector2Int tilePosition = new Vector2Int((int)spawnPosition.x, (int)spawnPosition.y);
                         if (EnvironmentManager.Instance.biomeMap.GetTileBiome(tilePosition) == biome)
                         {
-                            //GameObject go = Instantiate(TmpMonsters[tmp], spawnPosition, Quaternion.identity);
-                            //go.GetComponent<SpriteRenderer>().sortingOrder = 100;
-                            //SpawnedMonster.Add(go);
+                            GameObject go = Instantiate(monsters[monsterPrefabIdx++], spawnPosition, Quaternion.identity);
+                            if (monsterPrefabIdx == monsters.Count) monsterPrefabIdx = 0; //하나의 몬스터만 생성되는것을 방지하기 위해 
+                            go.GetComponent<SpriteRenderer>().sortingOrder = 100;
+                            SpawnedMonster[biome].Add(go);
                         }
                     }
                 }
 
             }
-            tmp++;
+        }
+    }
+
+    public void ClearSpawnedMonsters()
+    {
+        foreach (var mosterList in SpawnedMonster)
+        {
+            List<GameObject> SpawnedMonsters = mosterList.Value;
+            foreach (var monster in SpawnedMonsters)
+            {
+                DestroyObject(monster);
+            }
+            mosterList.Value.Clear();
         }
     }
 }
