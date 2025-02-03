4efc2ea0d200844f2057e37f62e3cd90b655f3c4
youjin43 <wlwlfdl3324@naver.com>
2025-02-03 23:05:01 +0900

    어둠정령 완성
    1. 스폰 : 모닥불 오브젝트 근처에서 밤 시간대에 2~3마리 출몰
    2. 모닥불 끄기 : 모닥불에 뛰어들어서 모닥불을 끄려 함
    3. 사망 : 플레이어에게 닿거나 공격을 받으면 사망

--- Assets/02_Script/Item/Campfire.cs
+++ Assets/02_Script/Item/Campfire.cs
@@ -1,27 +1,57 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal; // Light2D 사용 
 
 public class Campfire : MonoBehaviour
 {
-    public float maxDurability;
+    [Header("Durability")]
+    [SerializeField] float maxDurability = 15f;
     public float currentDurability;
 
-    float timeScale = 180f;
+    [Header("Light")]
+    [SerializeField] Light2D fireLight; // 불빛 조절용 Light 컴포넌트 - 인스펙터에서 할당
+    [SerializeField] Animator animator; // 인스펙터에서 할당
+    [SerializeField] float intensity;
+
+    [SerializeField] GameObject fireObj;
+
+    //float timeScale = 180f;
 
     void Start()
     {
         currentDurability = maxDurability;
     }
 
-    
     void Update()
     {
-        currentDurability -= Time.deltaTime * timeScale;
-
-        if (currentDurability <= 0)
+        if(currentDurability > 0)
         {
-            Vector2Int pos = new Vector2Int((int)transform.position.x, (int)transform.position.y);
-            EnvironmentManager.Instance.objectMap.ClearTiles(pos, 1, 1);
-            Destroy(gameObject);
+            //currentDurability -= Time.deltaTime * timeScale;
+            currentDurability -= Time.deltaTime;
+            UpdateFireLight();
+
+            if (currentDurability <= 0)
+            {
+                fireLight.intensity = 0;
+                //Vector2Int pos = new Vector2Int((int)transform.position.x, (int)transform.position.y);
+                //EnvironmentManager.Instance.objectMap.ClearTiles(pos, 1, 1);
+                //Destroy(gameObject);
+            }
+        }
+
+    }
+
+    public void AddDurability(float amount)
+    {
+        currentDurability += amount;
+    }
+
+    void UpdateFireLight()
+    {
+        if (fireLight != null)
+        {
+            intensity = Mathf.Lerp(0, 1, currentDurability / maxDurability); // 0~1 사이 값 조절
+            fireLight.intensity = intensity;
+            animator.SetFloat("intensity", intensity);
         }
     }
 }
---
+++ Assets/02_Script/Monster/NightTime/DarkSpiritSpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 모닥불(campFire)가 컴포넌트로 가지고 있음 
+/// </summary>
+public class DarkSpiritSpawner : MonoBehaviour
+{
+    public GameObject darkSpiritPrefab;
+
+    public int minSpawnCount = 2; 
+    public int maxSpawnCount = 4;
+
+    public float spawnRadius = 10f;
+
+    public float minSpawnDelay = 1f; // 최소 스폰 딜레이
+    public float maxSpawnDelay = 5f; // 최대 스폰 딜레이
+
+    void Start()
+    {
+        StartCoroutine(SpawnDarkSpirits());
+    }
+
+    IEnumerator SpawnDarkSpirits()
+    {
+        int spawnCount = Random.Range(minSpawnCount, maxSpawnCount + 1);
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            float spawnDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
+            yield return new WaitForSeconds(spawnDelay); // 랜덤한 시간 기다림
+
+            Vector2 spawnPosition = (Vector2)transform.position + Random.insideUnitCircle * spawnRadius;
+            // 어둠 정령 생성 후 타겟을 나(모닥불)로 설정 
+            Instantiate(darkSpiritPrefab, spawnPosition, Quaternion.identity).GetComponent<DarkSprit>().targetCampfire = GetComponent<Campfire>(); 
+        }
+    }
+}
