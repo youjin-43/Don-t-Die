9e2004a277bd5baceebfc0e1eae2440958a0cc27
youjin43 <wlwlfdl3324@naver.com>
2025-02-03 14:15:26 +0900

    게임 시작시 몬스터 스폰 되도록

--- Assets/02_Script/GameManager.cs
+++ Assets/02_Script/GameManager.cs
@@ -30,9 +30,20 @@ public class GameManager : MonoBehaviour
         Init();
     }
 
+    private void Start()
+    {
+        GenMonsters();
+    }
+
     public Transform PlayerTransform; //인스펙터에서 할당해야함
     public Vector3 GetPlayerPos()
     {
         return PlayerTransform.position;
+    }
+
+    void GenMonsters()
+    {
+        Debug.Log("GenMonsters 호출됨 ");
+        MonsterSpawnManager.Instance.InitializeBiomeMonsters();
     }
 }
